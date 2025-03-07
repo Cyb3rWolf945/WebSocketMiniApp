08d7bda0e29ed9c6bda98b10ac15fc0fe9f3bc95
Joao Silva <jotal96@gmail.com>
2025-03-07 22:50:32 +0000

    adicionada biblioteca da Microsoft signalR. da forma atual já consegue ouvir todas as mensagens que sejam anunciadas pelo BroadCast

--- src/components/ReactHubLibrary.js
+++ src/components/ReactHubLibrary.js
@@ -1,5 +1,45 @@
+import {useEffect, useState} from "react";
+import * as signalR from "@microsoft/signalr";
+
 export default function ReactHubLibrary() {
+    const [connection, setConnection] = useState(null);
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        const newConnection = new signalR.HubConnectionBuilder()
+            .withUrl("https://jsamsignal.azurewebsites.net/signal", {
+                skipNegotiation: false, // Optional based on your server config
+                transport: signalR.HttpTransportType.WebSockets,
+            })
+            .withAutomaticReconnect()
+            .build();
+
+        setConnection(newConnection);
+    }, []);
+
+    useEffect(() => {
+        if (connection) {
+            connection
+                .start()
+                .then(() => {
+                    console.log("Connected to SignalR hub");
+
+                    // Listening for messages
+                    connection.on("BroadCast", (message) => {
+                        console.log(message);
+                        setMessages((prevMessages) => [...prevMessages, message]);
+                    });
+                })
+                .catch((error) => console.error("Connection error:", error));
+        }
+    }, [connection]);
 
 
-    return <>Ola</>;
+    return <>
+        <ul>
+            {messages.map((message, index) => (
+                <li key={index}>{message}</li>
+            ))}
+        </ul>
+    </>;
 }
